258f8869843f28976028db25e6fe465aa74c939b
nyuszika7h <nyuszika7h@gmail.com>
2021-03-17 14:19:07 +0100

    Refactor code into functions, add type hints and docstrings

--- getnonce.py
+++ getnonce.py
@@ -6,58 +6,60 @@ import shutil
 import subprocess
 import time
 import sys
+from typing import Optional
 
 import xmltodict
 
 
-os.environ['PATH'] = os.pathsep.join(['.', os.environ['PATH']])
-if not shutil.which('idevicediagnostics'):
-    print('[-] ERROR: idevicediagnostics not found. Please place the binary in your PATH or the same folder as the script and try again.')
-    sys.exit(1)
-
-
-def wait_for_device():
+def wait_for_device() -> None:
+    """Wait for a device that's connected over USB and unlocked."""
     print('[*] Waiting for USB device (make sure the device is unlocked)')
     while True:
         # Run a dummy command to make sure the device is connected and unlocked.
         # `idevice_id -l` doesn't appear to be enough.
         p = subprocess.run(['idevicediagnostics', 'diagnostics'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         if p.returncode == 0:
-            return True
+            return
         time.sleep(1)
 
 
-wait_for_device()
-print('[+] Getting ApNonce')
-#p = subprocess.run(['igetnonce'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#sys.stdout.write(p.stdout.decode('utf-8'))
-#if p.returncode != 0:
-#    print('[-] ERROR: ' + p.stdout.decode('utf-8'))
-#    sys.exit(1)
-p = subprocess.run(['idevicediagnostics', 'mobilegestalt', 'ApNonce'], stdout=subprocess.PIPE)
-xml = xmltodict.parse(p.stdout)
-try:
-    apnonce = f"{int.from_bytes(base64.b64decode(xml['plist']['dict']['dict']['data']), 'big'):x}"
-except KeyError:
-    print('[-] ERROR: Unable to read ApNonce')
-    sys.exit(1)
-else:
-    print(f'ApNonce = {apnonce}')
+def mobilegestalt_read_bytes(key: str, endianness: str) -> Optional[str]:
+    """Read bytes with the specified endianness from MobileGestalt and return it as a hex string."""
+    p = subprocess.run(['idevicediagnostics', 'mobilegestalt', key], stdout=subprocess.PIPE)
+    xml = xmltodict.parse(p.stdout)
+    try:
+        value = base64.b64decode(xml['plist']['dict']['dict']['data'])
+    except KeyError:
+        return None
+    return '{:x}'.format(int.from_bytes(value, endianness))
 
-print('[+] Rebooting device')
-p = subprocess.run(['idevicediagnostics', 'restart'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-if p.returncode != 0:
-    print('[-] ERROR: ' + p.stdout.decode('utf-8'))
-    sys.exit(1)
 
-wait_for_device()
-print('[+] Getting BootNonce')
-p = subprocess.run(['idevicediagnostics', 'mobilegestalt', 'BootNonce'], stdout=subprocess.PIPE)
-xml = xmltodict.parse(p.stdout)
-try:
-    bootnonce = f"{int.from_bytes(base64.b64decode(xml['plist']['dict']['dict']['data']), 'little'):x}"
-except KeyError:
-    print('[-] ERROR: Unable to read BootNonce')
-    sys.exit(1)
-else:
-    print(f'BootNonce = 0x{bootnonce}')
+if __name__ == '__main__':
+    os.environ['PATH'] = os.pathsep.join(['.', os.environ['PATH']])
+    if not shutil.which('idevicediagnostics'):
+        print('[-] ERROR: idevicediagnostics not found. Please place the binary in your PATH or the same folder as the script and try again.')
+        sys.exit(1)
+
+    wait_for_device()
+    print('[+] Getting ApNonce')
+    apnonce = mobilegestalt_read_bytes('ApNonce', 'big')
+    if apnonce:
+        print(f'ApNonce = {apnonce}')
+    else:
+        print('[-] ERROR: Unable to read ApNonce')
+        sys.exit(1)
+
+    print('[+] Rebooting device')
+    p = subprocess.run(['idevicediagnostics', 'restart'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if p.returncode != 0:
+        print('[-] ERROR: ' + p.stdout.decode('utf-8'))
+        sys.exit(1)
+
+    wait_for_device()
+    print('[+] Getting BootNonce')
+    bootnonce = mobilegestalt_read_bytes('BootNonce', 'little')
+    if bootnonce:
+        print(f'BootNonce = 0x{bootnonce}')
+    else:
+        print('[-] ERROR: Unable to read BootNonce')
+        sys.exit(1)
